e4bb6b0b5bee0588e3ab1b223ebd82d0c11f9d45
moikrom <mikram@gmail.com>
2023-03-03 16:00:44 +0700

    thunder client API

--- user/u_read.php
+++ user/u_read.php
@@ -1,10 +1,13 @@
 <?php
 
+$username = $_POST["username"];
+$password = $_POST["password"];
+
 // syntax menyambungkan ke database = mysqli_connect( "URL server" , "username" , "password" , "database")
 $koneksi = mysqli_connect("localhost", "root", "", "db_tes_catatan2");
 
 // mysqli_query (koneksinya , SQL nya)
-$sql = "SELECT id_user, username, hp, create_date FROM users ORDER BY username";
+$sql = "SELECT id_user, username, hp, create_date FROM users where username='$username' and `password`='$password' ";
 $result = mysqli_query($koneksi, $sql);
 
 
@@ -12,8 +15,20 @@ $result = mysqli_query($koneksi, $sql);
 $data = array();
 
 //  melakukan looping menggunakan while
-while ($row = mysqli_fetch_assoc($result)) {
-    $data[] = $row;
+// while ($row = mysqli_fetch_assoc($result)) {
+//     $data[] = $row;
+// }
+
+// Menampilkan Hasil
+
+if (mysqli_num_rows($result) > 0) {
+    $row =  mysqli_fetch_assoc($result);
+    $data["Message"] = "Success";
+    $data["Status"] = "200";
+    $data["user"] = $row;
+} else {
+    $data["Message"] = "Username or Password Wrong";
+    $data["Status"] = "404";
 }
 
 
